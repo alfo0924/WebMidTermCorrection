b5b9d6d49ea0550a211fb7dc0bdfbcae7e97c8ff
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-02 21:51:54 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,39 @@
+
+
+    const app = Vue.createApp({
+    data() {
+    return {
+    count11: 0, // Separate count for each image
+    // Add more count variables for other images if needed
+    count22:0,
+    count33:0,
+    count44:0,
+    count55:0,
+    count66:0,
+    count77:0,
+    count88:0,
+    count99:0,
+    count1010:0,
+    count1212:0,
+    count1111:0,
+    count01:0,
+    count02:0,
+    count03:0,
+    count04:0,
+    count05:0,
+    count06:0,
+    count07:0,
+    count08:0,
+    count09:0,
+    count10:0,
+    count011:0,
+    count012:0,
+    count013:0,
+
+
+
+}
+}
+});
+    app.mount('.row'); // Mount Vue app to .row element
+
